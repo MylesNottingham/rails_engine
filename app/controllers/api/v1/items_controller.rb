81a2cc48ae99eb74cbb72ce2dac131d150ca6891
Myles Nottingham <MylesNottingham@gmail.com>
2023-07-13 15:07:35 -0600

    Feat: Add methods

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,9 +1,31 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    render json: Item.all
+    render json: ItemSerializer.new(Item.all)
   end
 
   def show
-    render json: Item.find(params[:id])
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def create
+    render json: ItemSerializer.new(Item.create(item_params)), status: :created
+  end
+
+  def update
+    if params[:merchant_id] == nil || Merchant.exists?(params[:merchant_id])
+      render json: ItemSerializer.new(Item.update(params[:id], item_params))
+    else
+      render json: { errors: "Merchant not found" }, status: :not_found
+    end
+  end
+
+  def destroy
+    render json: ItemSerializer.new(Item.delete(params[:id])), status: :no_content
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
   end
 end
