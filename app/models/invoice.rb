b0a9084dfb871df6a455420a8f1fcb1e463bc6e2
Myles Nottingham <MylesNottingham@gmail.com>
2023-07-13 15:21:00 -0600

    Refactor: Delete uneccecary files

--- app/models/invoice.rb
+++
@@ -1,6 +0,0 @@
-class Invoice < ApplicationRecord
-  belongs_to :merchant
-  belongs_to :customer
-  has_many :transactions
-  has_many :invoice_items
-end
--- app/serializers/invoice_serializer.rb
+++
@@ -1,4 +0,0 @@
-class InvoiceSerializer
-  include JSONAPI::Serializer
-  attributes :customer_id, :merchant_id, :status
-end
