1aefae2d851c107e758417b7f2b5687f552d4c64
Myles Nottingham <MylesNottingham@gmail.com>
2023-07-14 00:47:41 -0600

    Feat: Add Item model class method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  def self.top_search(params)
+    if params[:name]
+      where("name ILIKE ?", "%#{params[:name]}%").first
+    elsif params[:min_price] && params[:max_price]
+      where("unit_price >= ? AND unit_price <= ?", params[:min_price].to_f, params[:max_price].to_f).order(:unit_price).first
+    elsif params[:min_price]
+      where("unit_price >= ?", params[:min_price].to_f).order(:unit_price).first
+    elsif params[:max_price]
+      where("unit_price <= ?", params[:max_price].to_f).order(:unit_price).last
+    end
+  end
 end
