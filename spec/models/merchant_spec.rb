eab5f2fbc5eaa14bba42cbcc7bf1f39daed41b5d
Myles Nottingham <MylesNottingham@gmail.com>
2023-07-13 23:26:30 -0600

    Test: Add test for class method to Merchant Model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Merchant, type: :model do
   describe "relationships" do
     it { should have_many(:items) }
   end
+
+  describe "class methods" do
+    it "#self.search" do
+      merchant_1 = create(:merchant, name: "Turing")
+      merchant_2 = create(:merchant, name: "Ring World")
+      merchant_3 = create(:merchant, name: "Earring World")
+
+      expect(Merchant.search({name: "ring"})).to eq([merchant_1, merchant_2, merchant_3])
+
+      expect(Merchant.search({name: "ear"})).to eq([merchant_3])
+      expect(Merchant.search({name: "ear"})).to_not eq([merchant_1, merchant_2])
+
+      expect(Merchant.search({name: "zzz"})).to eq([])
+      expect(Merchant.search({name: "zzz"})).to_not eq([merchant_1, merchant_2, merchant_3])
+    end
+  end
 end
